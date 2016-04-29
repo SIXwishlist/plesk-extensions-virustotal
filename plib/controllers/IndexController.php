9c3beb51f4405ea099283d73c6b7f4bd2c0f2af1
Oleg Neumyvakin <oneumyvakin@gmail.com>
2016-04-29 18:44:32 +0600

    Get domains from API

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -1,4 +1,6 @@
 <?php
+// plesk bin extension --create my-extension
+// plesk bin extension --register my-extension
 
 class IndexController extends pm_Controller_Action
 {
@@ -88,10 +90,18 @@ class IndexController extends pm_Controller_Action
 
     private function _getDomains()
     {
+        $domains = [];
         $sites_request = '<site><get><filter/><dataset><gen_info/></dataset></get></site>';
-        $subrs_request = '<webspace><get><filter/><dataset><gen_info/></dataset></get></webspace>';
+        $websp_request = '<webspace><get><filter/><dataset><gen_info/></dataset></get></webspace>';
         $api = pm_ApiRpc::getService();
+        // site->get->result->[ id, data -> gen_info ( [cr_date] , [name] , [ascii-name] , [status] => 0 , [dns_ip_address] , [htype] )
         $sites_response = $api->call($sites_request);
+        $websp_response = $api->call($websp_request);
+        $tmp_list = array_merge($sites_response->site->get->result, $websp_response->webspace->get->result);
+        
+        
+
+
         return $sites_response;
     }
 
