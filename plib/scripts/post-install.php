0a1426bca649bd2f1bfda7e0b86c037c27235db8
oneumyvakin <oneumyvakin@gmail.com>
2016-05-02 14:59:45 +0600

    Add periodic task. Next: report to promo. Next: Report page.

--- plib/scripts/post-install.php
+++ plib/scripts/post-install.php
@@ -1,2 +1,16 @@
 <?php
-// TODO: add implementation here
+
+pm_Context::init('plesk-extensions-virustotal');
+
+$tasks = pm_Scheduler::getInstance()->listTasks();
+foreach ($tasks as $task) {
+    if ('virustotal-periodic-task.php' == $task->getCmd()) {
+        pm_Settings::set('virustotal_periodic_task_id', $task->getId());
+        return;
+    }
+}
+$task = new pm_Scheduler_Task();
+$task->setSchedule(pm_Scheduler::$EVERY_DAY);
+$task->setCmd('virustotal-periodic-task.php');
+pm_Scheduler::getInstance()->putTask($task);
+pm_Settings::set('virustotal_periodic_task_id', $task->getId());
