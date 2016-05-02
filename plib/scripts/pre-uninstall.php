bc39bb7648a0c178d3b9a63c385ef997baa52808
oneumyvakin <oneumyvakin@gmail.com>
2016-05-02 15:01:54 +0600

    Add pre-uninstall. Next: report to promo. Next: Report page.

--- plib/scripts/pre-uninstall.php
+++ plib/scripts/pre-uninstall.php
@@ -1,2 +1,15 @@
 <?php
-// TODO: add implementation here
+
+pm_Context::init('plesk-extensions-virustotal');
+
+$taskId = pm_Settings::get('virustotal_periodic_task_id');
+if (!$taskId) {
+    return;
+}
+
+try {
+    $task = pm_Scheduler::getInstance()->getTaskById($taskId);
+    pm_Scheduler::getInstance()->removeTask($task);
+} catch (pm_Exception $e) {
+    echo $e->getMessage();
+}
