335c2994c64b80a8f545c19f16cda60ac15222b7
oneumyvakin <oneumyvakin@gmail.com>
2016-05-11 22:30:50 +0600

    [Minor] To remove all scheduled tasks on pre-uninstall it's better to use http://docs.plesk.com/en-US/12.5/extensions-reference/classes/pm_Scheduler.html#method_removeAllTasks

--- plib/scripts/pre-uninstall.php
+++ plib/scripts/pre-uninstall.php
@@ -2,16 +2,6 @@
 
 pm_Context::init('virustotal-site-checker');
 
-$taskId = pm_Settings::get('virustotal_periodic_task_id');
-if (!$taskId) {
-    return;
-}
-
-try {
-    $task = pm_Scheduler::getInstance()->getTaskById($taskId);
-    pm_Scheduler::getInstance()->removeTask($task);
-} catch (pm_Exception $e) {
-    echo $e->getMessage();
-}
+pm_Scheduler::getInstance()->removeAllTasks();
 
 pm_Settings::clean();
