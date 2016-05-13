032dc75d2a41d753206f5b1118382130d9f4fb1a
oneumyvakin <oneumyvakin@gmail.com>
2016-05-13 21:15:51 +0600

    Bugfix, unclosed tag

--- plib/resources/locales/ru-RU.php
+++ plib/resources/locales/ru-RU.php
@@ -29,7 +29,7 @@ $messages = array(
     'ofTotalDomains' => ' из всего проверенных доменов ',
     'totalReports' => 'Всего "плохих" отчетов: ',
     'lastScan' => 'последнее сканирование выполнено в ',
-    'about' => 'Это расширение использует публичное API <a rel="noopener noreferrer" target=\'_blank\' href=\'https://virustotal.com/\'>поставщика услуг/a>, чтобы проверить Ваши сайты на вредоносные скрипты. Запросы выполняются через ежедневную задачу, которую Вы можете найти в <a href="/admin/scheduler/tasks-list">Scheduled Tasks</a>',
+    'about' => 'Это расширение использует публичное API <a rel="noopener noreferrer" target=\'_blank\' href=\'https://virustotal.com/\'>поставщика услуг</a>, чтобы проверить Ваши сайты на вредоносные скрипты. Запросы выполняются через ежедневную задачу, которую Вы можете найти в <a href="/admin/scheduler/tasks-list">Scheduled Tasks</a>',
     'feedback' => 'Если у Вас есть вопросы Вы можете задать их на <a rel="noopener noreferrer" target="_blank" href="http://serverfault.com/questions/ask?tags=plesk+virustotal">serverfault.com</a> c тегами "plesk" и "virustotal"',
     'faq' => 'FAQ',
     'question1' => '<p><b>Q: Почему нет нотификации по e-mail?</b><br />A: Нет никаких гарантий, что сообщение электронной почты попадет к Вам в ящик.</p>',
